70c1f410d82cf7cf5fb82189148ae544e0e6230b
hala-najdawi <alnajdawihala@gmail.com git config --global user.name hala-najdawigit config --global user.email alnajdawihala@gmail.com>
2019-11-19 16:15:57 +0200

    finish python_exercises

---
+++ python_exercises.py
@@ -0,0 +1,91 @@
+"""
+print("Hello\n\t Hello\n\t\tHello")
+print("------------------EX2--------------------------")
+#EX2
+print("Orange Acadmy\n"*20)
+print("-----------------EX3---------------------------")
+x=float(1)
+y=float(2.8)
+z=float("3")
+w=float("4.2")
+print(x,y,z,w,sep='10')
+print("-----------------EX4---------------------------")
+num1=int(input("Enter the number1:))
+num2=int(input("Enter the number2:))
+print(num1*num2)
+print("-----------------EX5---------------------------")
+num1=int(input("Enter the number1:))
+num2=int(input("Enter the number2:))
+num3=int(input("Enter the number3:))
+num4=int(input("Enter the number4:))
+num5=int(input("Enter the number5:))
+print((num1+num2+num3+num4+num5)/5)
+print("-----------------EX6---------------------------")
+x=1
+y=2.8
+z=1j
+o="Orange"
+A="True"
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
+print(type(A))
+print("-----------------EX7---------------------------")
+x,y="orange",1
+x1,y1=100,-10
+print(x)
+print(y)
+print(x1)
+print(y1)
+"""
+print("-----------------EX8 and EX 10---------------------------")
+x5=10
+"""print(5x) A variable name must start with a letter or the underscore character"""
+print(x5)
+X_q="orange"
+"""print(X_q$) SyntaxError:$"""
+print(X_q)
+AB=12
+"""PRINT(A B) invalid syntax:Space"""
+print(AB)
+print("10"*10)
+print("-----------------EX 11---------------------------")
+#print("Hello World!")
+print("cheers,Mate!")#This is program
+"""Testing
+the code
+"""
+"""
+print("-----------------EX 12---------------------------")
+name=str(input("Enter your name:"))
+age=int(input("Enter your Age:"))
+Age_after_100=(100-age)+2019
+print(name,"You will be 100 years old in ",Age_after_100)
+"""
+"""
+print("-----------------EX 13---------------------------")
+base=int(input("Enter Base of a triangle:"))
+height=int(input("Enter Height of a triangle:"))
+area=0.5*base*height
+print("Aeea of the triangle",area)
+"""
+print("-----------------EX 14---------------------------")
+x=11
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(abs(x*-1))
+print(int(x))
+print(float(x))
+print(divmod(x,y))
+print(pow(x,y))
+print(x**y)
+print(x>y)
+print(x==y)
+print(x!=y)
+
